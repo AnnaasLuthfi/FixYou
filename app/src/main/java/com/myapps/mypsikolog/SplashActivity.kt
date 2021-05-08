59f1f4fc583e665f6ddd1a9df9a26e0801b32fe4
AnnaasLuthfi <annaasluthfi@gmail.com>
2021-05-08 10:22:15 +0700

    MyPsikolog

---
+++ app/src/main/java/com/myapps/mypsikolog/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.myapps.mypsikolog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        }, 2000)
+
+
+    }
+}
