679110f0f9d05b3dfb87545af683c0131f0befa6
AnnaasLuthfi <annaasluthfi@gmail.com>
2021-05-08 12:52:45 +0700

    MyPsikolog

--- app/src/main/java/com/myapps/mypsikolog/AfterSplashActivity.kt
+++ app/src/main/java/com/myapps/mypsikolog/AfterSplashActivity.kt
@@ -3,7 +3,7 @@ package com.myapps.mypsikolog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AfterSplash : AppCompatActivity() {
+class AfterSplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_after_splash)
