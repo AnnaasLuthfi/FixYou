efccc5f6a9b34b4ba38edfe518b34da821e1b3d7
AnnaasLuthfi <annaasluthfi@gmail.com>
2021-05-12 12:27:10 +0700

    MyPsikolog

--- app/src/main/java/com/myapps/mypsikolog/Journal1Activity.kt
+++ app/src/main/java/com/myapps/mypsikolog/Journal1Activity.kt
@@ -3,11 +3,11 @@ package com.myapps.mypsikolog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class Journal1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_journal1)
 
-        supportActionBar?.setIcon(R.drawable.ic_baseline_arrow_back_ios_24)
+        supportActionBar.hide()
     }
 }
