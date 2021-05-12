239ce9f5060da95a51e0505fce923ff0df63e2ef
AnnaasLuthfi <annaasluthfi@gmail.com>
2021-05-12 14:24:11 +0700

    MyPsikolog

--- app/src/main/java/com/myapps/mypsikolog/Journal2Activity.kt
+++ app/src/main/java/com/myapps/mypsikolog/Journal2Activity.kt
@@ -3,11 +3,10 @@ package com.myapps.mypsikolog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Journal1Activity : AppCompatActivity() {
+class Journal2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_journal1)
-
+        setContentView(R.layout.activity_journal2)
         supportActionBar?.hide()
     }
 }
