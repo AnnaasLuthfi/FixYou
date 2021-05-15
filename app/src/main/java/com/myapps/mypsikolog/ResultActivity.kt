090663155cede3713edd505d5f3af7495f1e37f3
AnnaasLuthfi <annaasluthfi@gmail.com>
2021-05-15 15:53:32 +0700

    MyPsikolog

--- app/src/main/java/com/myapps/mypsikolog/ResultActivity.kt
+++ app/src/main/java/com/myapps/mypsikolog/ResultActivity.kt
@@ -7,5 +7,7 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        supportActionBar?.hide()
     }
 }
